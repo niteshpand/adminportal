edccb09133a6b5a4a93faba4b4344f58752a8ef1
nitesh <pandeynitesh822@gmail.com>
2025-04-11 11:25:13 +0530

    created folder module component and other interfaces according to requirements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FeatherIconComponent } from './feather-icon/feather-icon.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [AppComponent, FeatherIconComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
   ],
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FeatherIconComponent } from './components/feather-icon/feather-icon.component';
+import { LayoutComponent } from './components/layout/layout.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    FeatherIconComponent,
+    LayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
